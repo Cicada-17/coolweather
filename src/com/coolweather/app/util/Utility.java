42ae564ae1f1a36402def40596effdd5c0c24404
liuzhao <liuzhao0010@gmail.com>
2016-10-19 18:46:47 +0800

    家里最后一次19日

--- src/com/coolweather/app/util/Utility.java
+++ src/com/coolweather/app/util/Utility.java
@@ -1,5 +1,7 @@
 package com.coolweather.app.util;
 
+import com.coolweather.app.model.Province;
+
 import android.text.TextUtils;
 
 public class Utility {
@@ -8,8 +10,20 @@ public class Utility {
 	{
 		if(!TextUtils.isEmpty(response))
 		{
-			String [] allProvince = response.split(",");
-			if()
+			String [] allProvinces = response.split(",");
+			if(allProvinces != null && allProvinces.length > 0)
+			{
+				for(String p : allProvinces)
+				{
+					String [] array = p.split("\\|");
+					Province province = new Province();
+					province.setProvinceCode(array[0]);
+					province.setProvinceName(array[1]);
+					coolweatherDB.saveProvince(province);
+				}
+				return true;
+			}
 		}
+		return false;
 	}
 }
